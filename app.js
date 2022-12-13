a89e900081ce20ada481122fe82a8ddce9013388
souma2half1 <souma1441085@gmail.com>
2022-12-13 11:17:59 +0000

    12/13

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-let orange = 100;
-let apple = 120;
-
-if(orange < apple){
-  alert('みかんの値段がりんごより安い');
-} else if(orange == apple){
-  alert('みかんとりんごが同じ値段');
-} else{
-  alert('みかんの値段がりんごより高い');
-}
+var nickname = "taro"
+console.log(nickname)
+var nickname = "ichiro"
+console.log(nickname)
